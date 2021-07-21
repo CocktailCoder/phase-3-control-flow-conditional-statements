804811e9e7fb2591de2059a800b9e7fee65a5b53
cocktailcoder <iokreddit@protonmail.com>
2021-07-20 21:43:27 -0400

    commit tree 63c6e8ba11103d75258a64ad265b1e95cb8fe94d

--- control_flow.rb
+++ control_flow.rb
@@ -1,6 +1,9 @@
 def admin_login(username, password)
-  # your code here
-end
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 
 def hows_the_weather(temperature)
   # your code here
